b0597333189c2e5d5f9a42ee3c73891cae080912
Jake Pitman <jakeseanp@gmail.com>
2023-09-20 19:55:08 +1200

    Add glb support to webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            outputPath: "assets/models/",
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
